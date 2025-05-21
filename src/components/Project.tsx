ba358f56478f105784343b103df30db65ad5d724
albanmuhammad <albanmuhammad07@gmail.com>
2025-05-21 14:48:48 +0700

    add link and github

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import TechStackCard from './TechStackCard';
+import { FaGithub, FaGlobe, } from 'react-icons/fa';
 
 const Page = () => {
     const [activeTab, setActiveTab] = useState<'mobile' | 'web'>('web');
@@ -13,6 +14,8 @@ const Page = () => {
             mobile: false,
             shortDescription: 'Web Portal for UPTD Kertasmaya Indramyu',
             description: 'Full-featured patient tracking and report system...',
+            link: 'https://restuinvestama.com/',
+            github: '-',
             techStack: ['React', 'HTML', 'Tailwind CSS', 'Next JS', 'TypeScript'],
             foto: '/uptdkertasmaya.png',
         },
@@ -21,6 +24,8 @@ const Page = () => {
             mobile: false,
             shortDescription: 'Company Profile for PT. Restu Inti Nusa Abadi',
             description: 'Custom built ERP for logistics and inventory...',
+            link: 'https://puskesmaskertasemaya.indramayukab.go.id/',
+            github: '-',
             techStack: ['React', 'HTML', 'CSS'],
             foto: '/restuinvestama.png',
         },
@@ -29,6 +34,8 @@ const Page = () => {
             mobile: true,
             shortDescription: 'Mobile Travel Booking Application',
             description: 'Lightweight mobile PWA for field health agents...',
+            link: '-',
+            github: 'https://github.com/albanmuhammad/rombis_lajon',
             techStack: ['Flutter'],
             foto: '/rombisLajon.jpeg',
         },
@@ -42,19 +49,17 @@ const Page = () => {
 
             {/* Tabs */}
             <div className="flex gap-4 my-6">
-                 <button
+                <button
                     onClick={() => setActiveTab('web')}
-                    className={`px-4 py-2 rounded-full border transition ${
-                        activeTab === 'web' ? 'bg-black text-white' : 'bg-white text-black'
-                    }`}
+                    className={`px-4 py-2 rounded-full border transition ${activeTab === 'web' ? 'bg-black text-white' : 'bg-white text-black'
+                        }`}
                 >
                     Web
                 </button>
                 <button
                     onClick={() => setActiveTab('mobile')}
-                    className={`px-4 py-2 rounded-full border transition ${
-                        activeTab === 'mobile' ? 'bg-black text-white' : 'bg-white text-black'
-                    }`}
+                    className={`px-4 py-2 rounded-full border transition ${activeTab === 'mobile' ? 'bg-black text-white' : 'bg-white text-black'
+                        }`}
                 >
                     Mobile
                 </button>
@@ -73,16 +78,34 @@ const Page = () => {
                                 alt={project.companyName}
                                 layout="fill"
                                 objectFit="contain"
-                                // className='scale-90'
+                            // className='scale-90'
                             />
                         </div>
-                        <div className="mb-2">
+                        <div className="mb-2 flex flex-col">
                             <h3 className="text-lg font-bold text-black">{project.companyName}</h3>
                             <p className="text-lg font-semibold text-black mt-2">{project.shortDescription}</p>
+                            <div className='flex mt-2 gap-3'>
+                                <a
+                                    href={project.link}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                    className='flex items-center gap-6'
+                                >
+                                    <FaGlobe className='text-3xl text-gray-700' />
+                                </a>
+                                <a
+                                    href={project.github}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                    className='flex items-center gap-6'
+                                >
+                                    <FaGithub className='text-3xl text-gray-700' />
+                                </a>
+                            </div>
                         </div>
                         <div className='flex flex-wrap gap-2 mt-2'>
                             {project.techStack.map((tech, idx) => (
-                                <TechStackCard key={idx} tech={tech}/>
+                                <TechStackCard key={idx} tech={tech} />
                             ))}
                         </div>
                     </div>
